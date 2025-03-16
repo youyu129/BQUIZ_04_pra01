a7df5d7108e8c51ec1c8a793a0e1d25731102325
youyu <129youyu@gmail.com>
2025-03-17 07:12:17 +0800

    匯出資料表types items

--- items.sql
+++ items.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-03-17 00:11:30
+-- 產生時間： 2025-03-17 00:11:36
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.0.30
 
@@ -24,40 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `types`
+-- 資料表結構 `items`
 --
 
-CREATE TABLE `types` (
+CREATE TABLE `items` (
   `id` int(10) UNSIGNED NOT NULL,
+  `big` int(10) UNSIGNED NOT NULL,
+  `mid` int(10) UNSIGNED NOT NULL,
+  `no` text NOT NULL,
   `name` text NOT NULL,
-  `big_id` int(10) UNSIGNED NOT NULL
+  `price` int(10) UNSIGNED NOT NULL,
+  `s` text NOT NULL,
+  `q` int(10) UNSIGNED NOT NULL,
+  `img` text NOT NULL,
+  `intro` text NOT NULL,
+  `sh` int(1) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- 傾印資料表的資料 `types`
---
-
-INSERT INTO `types` (`id`, `name`, `big_id`) VALUES
-(1, '流行皮件', 0),
-(2, '流行鞋區', 0),
-(3, '流行飾品', 0),
-(4, '背包', 0),
-(5, '男用皮件', 1),
-(6, '女用皮件', 1),
-(7, '少女鞋區', 2),
-(8, '紳士流行鞋區', 2),
-(9, '時尚手錶', 3),
-(10, '時尚珠寶', 3),
-(11, '背包', 4);
 
 --
 -- 已傾印資料表的索引
 --
 
 --
--- 資料表索引 `types`
+-- 資料表索引 `items`
 --
-ALTER TABLE `types`
+ALTER TABLE `items`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -65,10 +56,10 @@ ALTER TABLE `types`
 --
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `types`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `items`
 --
-ALTER TABLE `types`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+ALTER TABLE `items`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
