15f7a458b4b21e44c04fb3d6a715bdfcf8e46b93
youyu <129youyu@gmail.com>
2025-03-16 21:38:35 +0800

    完成後台商品管理畫面

--- back/th.php
+++ back/th.php
@@ -1,1 +1,69 @@
-th
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+    新增大分類
+    <input type="text" name="big" id="big">
+</div>
+<div class="ct">
+    新增中分類
+    <select name="selectBig" id="selectBig">
+        <option value="1">1</option>
+        <option value="2">2</option>
+    </select>
+    <input type="text" name="mid" id="mid">
+    <button>新增</button>
+</div>
+<table class="all">
+    <tr class="tt">
+        <td>流行皮件</td>
+        <td class="ct">
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr class="ct pp">
+        <td>女用皮件</td>
+        <td>
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+
+<h2 class="ct">商品管理</h2>
+<div class="ct">
+    <button>新增商品</button>
+</div>
+<div class="ct">
+    <select name="" id="">
+        <option value=""></option>
+    </select>
+</div>
+<table class="all">
+    <tr class="ct">
+        <td class="tt">編號</td>
+        <td class="tt">商品名稱</td>
+        <td class="tt">庫存量</td>
+        <td class="tt">狀態</td>
+        <td class="tt">操作</td>
+    </tr>
+    <tr class="ct">
+        <td class="pp"></td>
+        <td class="pp"></td>
+        <td class="pp"></td>
+        <td class="pp"></td>
+        <td class="pp">
+            <button>修改</button>
+            <button>刪除</button>
+            <button>上架</button>
+            <button>下架</button>
+        </td>
+    </tr>
+</table>
