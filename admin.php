84f52019f99e6d0ef29f8434e075004d0c08bec3
youyu <129youyu@gmail.com>
2025-03-16 19:00:52 +0800

    完成後台管理權限設定

--- admin.php
+++ admin.php
@@ -1,5 +1,8 @@
 <?php
 include_once "./api/db.php";
+$user=$Admin->find(['acc'=>$_SESSION['admin']]);
+$userP=unserialize($user['p']);
+// dd($userP);
 ?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -27,11 +30,23 @@ include_once "./api/db.php";
         <div id="left" class="ct">
             <div style="min-height:400px;">
                 <a href="?do=admin">管理權限設置</a>
-                <a href="?do=th">商品分類與管理</a>
-                <a href="?do=order">訂單管理</a>
-                <a href="?do=mem">會員管理</a>
-                <a href="?do=bot">頁尾版權管理</a>
-                <a href="?do=news">最新消息管理</a>
+                <?php
+                if(in_array(1,$userP)){
+                  echo "<a href='?do=th'>商品分類與管理</a>";  
+                }
+                if(in_array(2,$userP)){
+                    echo "<a href='?do=order'>訂單管理</a>";
+                }
+                if(in_array(3,$userP)){
+                    echo "<a href='?do=mem'>會員管理</a>";
+                }
+                if(in_array(4,$userP)){
+                    echo "<a href='?do=bot'>頁尾版權管理</a>";
+                }
+                if(in_array(5,$userP)){
+                    echo "<a href='?do=news'>最新消息管理</a>";
+                }
+                ?>
                 <a href="./api/logout.php" style="color:#f00;">登出</a>
             </div>
         </div>
