c4fb82d478dd3d8ef44effb8387be2bfd629041f
youyu <129youyu@gmail.com>
2025-03-16 20:49:45 +0800

    將後台頁尾版權修改功能改為在當前頁處理

--- back/bot.php
+++ back/bot.php
@@ -1,25 +1,22 @@
-<h2 class="ct">編輯頁尾版權區</h2>
-<table class="all">
-    <tr>
-        <td class="tt ct">頁尾宣告內容</td>
-        <td class="pp ct">
-            <input type="text" name="text" id="text" value="<?=$Bottom->find(1)['text'];?>">
-        </td>
-    </tr>
-</table>
-<div class="ct">
-    <button onclick='edit()'>編輯</button>
-    <button onclick='reset()'>重置</button>
-</div>
-
-<script>
-function edit() {
-    let text = $("#text").val()
-    $.post("./api/edit_bottom.php", {
-        text
-    }, (res) => {
-        console.log('res', res);
-        location.reload()
-    })
+<?php
+if(isset($_POST['id'])){
+    $Bottom->save($_POST);
 }
-</script>
+?>
+
+<form action="?do=bot" method="post">
+    <h2 class="ct">編輯頁尾版權區</h2>
+    <table class="all">
+        <tr>
+            <td class="tt ct">頁尾宣告內容</td>
+            <td class="pp ct">
+                <input type="text" name="text" id="text" value="<?=$Bottom->find(1)['text'];?>">
+            </td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="hidden" name="id" value="1">
+        <input type="submit" value="編輯">
+        <input type="reset" value="重置">
+    </div>
+</form>
